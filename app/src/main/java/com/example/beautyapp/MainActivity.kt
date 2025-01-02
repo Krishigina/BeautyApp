ef54e4862c9b63e13b6ace1eb9b6b287c721478d
Krishigina <krishigina@gmial.com>
2025-01-03 02:46:00 +0300

    changes

--- app/src/main/java/com/example/beautyapp/MainActivity.kt
+++ app/src/main/java/com/example/beautyapp/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.beautyapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.beautyapp.databinding.ActivitySplashBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val ivButtonArrow = findViewById<ImageView>(R.id.ivButtonArrow)
+        ivButtonArrow.setOnClickListener {
+            Intent(this, LoginActivity::class.java).also {
+                startActivity(it)
+            }
         }
     }
 }
--- app/src/main/java/com/example/beautyapp/SplashActivity.kt
+++ app/src/main/java/com/example/beautyapp/SplashActivity.kt
@@ -1,15 +1,17 @@
 package com.example.beautyapp
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.beautyapp.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,11 +20,6 @@ class SplashActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val ivButtonArrow = findViewById<ImageView>(R.id.ivButtonArrow)
-        ivButtonArrow.setOnClickListener{
-
         }
     }
 }
