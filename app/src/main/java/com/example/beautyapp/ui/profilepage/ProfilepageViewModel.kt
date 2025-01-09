6834d5097780a812f30121467bcb3358e0a949aa
Krishigina <krishigina@gmial.com>
2025-01-09 17:05:18 +0300

    Transfer to firebase firestore

--- app/src/main/java/com/example/beautyapp/ui/profilepage/ProfilepageViewModel.kt
+++ app/src/main/java/com/example/beautyapp/ui/profilepage/ProfilepageViewModel.kt
@@ -4,9 +4,8 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.firebase.Firebase
-import com.google.firebase.auth.auth
-import com.google.firebase.database.database
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ProfilepageViewModel : ViewModel() {
 
@@ -15,27 +14,30 @@ class ProfilepageViewModel : ViewModel() {
     private val _email: MutableLiveData<String> = MutableLiveData("")
     val email: LiveData<String> = _email
 
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+
     init {
-        _email.value = Firebase.auth.currentUser?.email
+        _email.value = auth.currentUser?.email
 
-        Firebase.auth.uid?.let { uid ->
-            Firebase.database.getReference("Users").child(uid).get().addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-
-                    val snapshot = task.result
-
-                    if (snapshot != null && snapshot.exists()) {
-                        val username = snapshot.child("username").getValue(String::class.java)
-
-                        _profileName.value = username ?: "Имя не установлено"
+        auth.currentUser?.uid?.let { uid ->
+            firestore.collection("Users")
+                .document(uid)
+                .get()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val document = task.result
+                        if (document != null && document.exists()) {
+                            val username = document.getString("username")
+                            _profileName.value = username ?: "Имя не установлено"
+                        } else {
+                            _profileName.value = "Пользователь не найден"
+                        }
                     } else {
-                        _profileName.value = "Пользователь не найден"
+                        _profileName.value = "Ошибка при загрузке данных"
+                        Log.e("Firebase", "Ошибка при получении данных", task.exception)
                     }
-                } else {
-                    _profileName.value = "Ошибка при загрузке данных"
-                    Log.e("Firebase", "Ошибка при получении данных", task.exception)
                 }
-            }
         }
     }
 }
--- app/src/main/java/com/example/beautyapp/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/beautyapp/ui/register/RegisterViewModel.kt
@@ -5,12 +5,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.beautyapp.utils.SharedPrefsManager
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RegisterViewModel : ViewModel() {
 
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
-    private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
 
     private val _registrationResult = MutableLiveData<Boolean>()
     val registrationResult: LiveData<Boolean> get() = _registrationResult
@@ -35,19 +35,19 @@ class RegisterViewModel : ViewModel() {
                     val currentUser = auth.currentUser
                     currentUser?.let {
                         val userInfo = hashMapOf(
+                            "uid" to it.uid,
                             "email" to email,
-                            "username" to name,
-                            "password" to password
+                            "username" to name
                         )
-                        database.getReference("Users")
-                            .child(it.uid)
-                            .setValue(userInfo)
-                            .addOnCompleteListener { databaseTask ->
-                                if (databaseTask.isSuccessful) {
+                        firestore.collection("Users")
+                            .document(it.uid)
+                            .set(userInfo)
+                            .addOnCompleteListener { firestoreTask ->
+                                if (firestoreTask.isSuccessful) {
                                     _registrationResult.value = true
                                     SharedPrefsManager.setUserAuthorized(true)
                                 } else {
-                                    _errorMessage.value = "Ошибка сохранения данных: ${databaseTask.exception?.message}"
+                                    _errorMessage.value = "Ошибка сохранения данных: ${firestoreTask.exception?.message}"
                                     _registrationResult.value = false
                                 }
                             }
